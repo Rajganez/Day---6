189ad07b143e3d76fa7a1681ba97a2f18d9824fc
Rajganez <ganezsouth@live.com>
2024-02-24 22:03:54 +0530

    day 6

--- Array methods.js
+++ Array methods.js
@@ -12,20 +12,13 @@ xhr.onload = function() {
    //  return value.name.common;
    // }
    // Get all the countries with a population of less than 2 lakhs using Filter function
-   // if(value.population < 200000){
-   //    return value.name.common;
-   // }      
-   // }
-// let fil = arr.filter(asia);
-// console.log(fil);
+   if(value.population < 200000){
+      return value.name.common;
+   }      
+   }
+let fil = arr.filter(asia);
+console.log(fil);
 // Print the total population of countries using reduce function
-let totalPopulation = (pop1,pop2) =>{
-   return value.pop1;
-}
-let pop = arr.reduce(totalPopulation,asia);
-console.log(pop);
-
-}
 }
 
 
